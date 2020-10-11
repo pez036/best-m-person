117655b594edb8741447e3dcd6b78fd5db68034e
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-10-11 01:21:18 -0700

    still buggy

--- Main.java
+++ Main.java
@@ -6,12 +6,35 @@ public class Main
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-
-		Set<Integer> nums = new HashSet<>();
+		Set<Integer> ids = new HashSet<>();
+		Person[] bestM = new Person[m];
+		int bestCount = 0;
 		for (int i = 0; i < n; ++i) {
-			
+			int id = sc.nextInt();
+			int score = sc.nextInt();
+			if (!ids.contains(id)) {
+				ids.add(id);
+				bestCount++;
+				bestM[bestCount] = (new Perseon(id, score));
+			} else {
+				for (int j = 0; j < bestCount; ++j) {
+					if (bestM[j].id == id){
+						bestM[j].score += score;
+					}
+				}
+			}
 		}
-	
+
 		System.out.println(ans);
 	}
+	class Person {
+		public int id;
+		public int score;
+
+		public Person(int id, int score) {
+			this.id = id;
+			this.score = score;
+		}
+	}
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-//TODO
+Given a group of people submiting their score. (each person can submit more than once, and scores will accumulate) 
