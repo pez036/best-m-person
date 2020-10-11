e4011d8b4d424b6edd1856e4b5d38674ce76dcb9
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-10-11 11:13:32 -0700

    sorting and README

--- Main.java
+++ Main.java
@@ -6,35 +6,39 @@ public class Main
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		Set<Integer> ids = new HashSet<>();
-		Person[] bestM = new Person[m];
-		int bestCount = 0;
+		if (m > n) {
+			throw new IllegalArgumentException("invalid input");
+		}
+		Map<Integer, Integer> pairs = new HashMap<>();
 		for (int i = 0; i < n; ++i) {
 			int id = sc.nextInt();
 			int score = sc.nextInt();
-			if (!ids.contains(id)) {
-				ids.add(id);
-				bestCount++;
-				bestM[bestCount] = (new Perseon(id, score));
+			if (pairs.containsKey(id)) {
+				pairs.replace(id, pairs.get(id)+score);
 			} else {
-				for (int j = 0; j < bestCount; ++j) {
-					if (bestM[j].id == id){
-						bestM[j].score += score;
-					}
-				}
+				pairs.put(id, score);
 			}
 		}
 
-		System.out.println(ans);
-	}
-	class Person {
-		public int id;
-		public int score;
-
-		public Person(int id, int score) {
-			this.id = id;
-			this.score = score;
+		int[][] list = new int[pairs.size()][2];
+		int i = 0;
+		for (Map.Entry<Integer, Integer> entry: pairs.entrySet()) {
+			list[i][0] = entry.getKey();
+			list[i][1] = entry.getValue();
+			++i;
 		}
+		
+		Arrays.sort(list, new Comparator<int[]>() {
+    		public int compare(int[] a, int[] b) {
+        		return Integer.compare(b[1], a[1]);
+    		}
+		});
+
+		for (int j  = 0; j < m; ++j) {
+            System.out.print(list[j][0]);
+			System.out.print(" ");
+        }
 	}
+
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Given a group of people submiting their score. (each person can submit more than once, and scores will accumulate) 
+Input:
+n m
+id0 score0
+id1 score1
+...
+idn scoren
+1. a group of people submiting their score
+2. total number of inputs n
+3. number of people with higest score m.
+4. (each person can submit more than once, and scores will accumulate)
+
+Output:
+m ids separated by a space.
+first m people's id ranking from highest to lower score.
+
+Example:
+We want the best 1 person with highest score. There are in total 3 submits. Alex(id = 0) submits 2 times with score 1 and 2. Bob(id = 1) submits 1 time with score 1.
+    Input:
+    3 1
+    0 1
+    0 2
+    1 1
+    Output:
+    0
