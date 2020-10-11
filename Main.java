fbf56a37429339e5133682d14b6867d97491a8c1
Alex Zhang <alexzhang@macbook-pro.attlocal.net>
2020-10-10 21:35:23 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String ans = "";
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		Set<Integer> nums = new HashSet<>();
+		for (int i = 0; i < n; ++i) {
+			
+		}
+	
+		System.out.println(ans);
+	}
+}
